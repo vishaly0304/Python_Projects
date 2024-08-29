611c10ec63788d8e8ce7128ca89764fb9385f76f
vishal <vishaly0304@gmail.com>
2024-08-29 20:28:08 +0530

    pdf to audio book code added

---
+++ pdf_to_audio_book/pdf_to_audio_book.py
@@ -0,0 +1,21 @@
+# Importing Python Modules
+from PyPDF2 import PdfReader
+import pyttsx3
+
+path="example.pdf" # Pdf file path
+
+Speaker=pyttsx3.init()
+PDF_Reader=PdfReader(path)
+
+text=""
+
+# Read the PDF and extract text from all pages
+for page_num, page in enumerate(PDF_Reader.pages):
+    text += page.extract_text()
+    Speaker.say(text)
+    Speaker.runAndWait()
+Speaker.stop()
+
+# Save to mp3 file format
+Speaker.save_to_file(text,"pdf_book.mp3")
+Speaker.runAndWait()
